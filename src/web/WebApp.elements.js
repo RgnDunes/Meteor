540ecc2ec00433288223737145bf68e979ee2496
RgnDunes <singh.divyansh1802@gmail.com>
2021-07-05 23:35:29 +0530

    fix navbar scrolling
    DESCRIPTION : On scroling page navbar scrolls up leaving a white gap if menubar was open.

--- src/web/WebApp.elements.js
+++ src/web/WebApp.elements.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: rgb(33, 33, 33);
   color: white;
-  height: 100vh;
+  height: 100%;
 `;
--- src/web/components/Navbar/Navbar.elements.js
+++ src/web/components/Navbar/Navbar.elements.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const NavContainer = styled.div`
+  position: sticky;
+  top: 0;
   padding: 20px;
   border: none;
   border-bottom: 2px solid lightgray;
@@ -8,6 +10,8 @@ export const NavContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   /* height: 80px; */
+  z-index: 999;
+  background-color: rgb(33, 33, 33);
 `;
 
 export const NavContainerSection = styled.div``;
