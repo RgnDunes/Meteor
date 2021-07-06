d064495af24a523974a1a1134c50d3aa2e2791a9
RgnDunes <singh.divyansh1802@gmail.com>
2021-07-07 02:24:54 +0530

    add Google Auth using firebase

--- src/web/pages/NavSlider/NavSlider.js
+++ src/web/pages/NavSlider/NavSlider.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { Container, NavSliderRow, NavSliderButton } from "./NavSlider.elements";
 import { Link } from "react-router-dom";
+import { auth, provider } from "../../../firebase";
+import { useStateValue } from "../../../StateProvider";
+import { actionTypes } from "../../../reducer";
 
 const NavSlider = ({ sliderOpen }) => {
+  const [state, dispatch] = useStateValue();
+
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
     <Container sliderOpen={sliderOpen}>
       <Link to="/home">
@@ -11,13 +30,15 @@ const NavSlider = ({ sliderOpen }) => {
       <Link to="/dashboard">
         <NavSliderRow>Dashboard</NavSliderRow>
       </Link>
-      <Link to="mailto:singh.divyansh1802@gmail.com">
+      <a href="mailto:singh.divyansh1802@gmail.com">
         <NavSliderRow>Contact Us</NavSliderRow>
-      </Link>
-      <Link to="https://rgndunes-v3.web.app/">
+      </a>
+      <a href="https://rgndunes-v3.web.app/">
         <NavSliderRow>Developers</NavSliderRow>
-      </Link>
-      <NavSliderButton>Login / Register</NavSliderButton>
+      </a>
+      <NavSliderButton onClick={() => signIn()}>
+        Login / Register
+      </NavSliderButton>
     </Container>
   );
 };
